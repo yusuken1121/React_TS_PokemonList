7050848028bda2a77ff52762d7f47ef18a9d8227
yusuken1121 <yusuken1121@gmail.com>
2024-06-05 15:49:40 -0700

    ✨ Creates custom hook

---
+++ src/provider/getPokeContext.tsx
@@ -0,0 +1,21 @@
+import { Dispatch, FC, createContext, useState } from "react";
+import { ChildrenType } from "../types/childrenType";
+import { PokemonInfoType } from "../types/pokemonType";
+interface pokemonContext {
+  pokemons: (PokemonInfoType | null)[];
+  setPokemons: Dispatch<React.SetStateAction<(PokemonInfoType | null)[]>>;
+}
+const defaultPokemonCtx = {
+  pokemons: [],
+  setPokemons: () => {},
+};
+
+export const GetPokeContext = createContext<pokemonContext>(defaultPokemonCtx);
+export const GetPokeProvider: FC<ChildrenType> = ({ children }) => {
+  const [pokemons, setPokemons] = useState<Array<PokemonInfoType | null>>([]);
+  return (
+    <GetPokeContext.Provider value={{ pokemons, setPokemons }}>
+      {children}
+    </GetPokeContext.Provider>
+  );
+};
