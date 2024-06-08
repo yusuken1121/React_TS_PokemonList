d65a123abad75ff183fff8e41c038a64327e7667
yusuken1121 <yusuken1121@gmail.com>
2024-06-08 13:34:03 -0700

    ⚡️ Improve CSS of inside of the card

--- src/components/molecules/PokemonCard/PokemonCard.tsx
+++ src/components/molecules/PokemonCard/PokemonCard.tsx
@@ -13,7 +13,7 @@ export const PokemonCard: FC<PokemonInfoType> = ({ image, pokeName, id }) => {
   const { setShowModal } = useContext(PokemonCardModalContext);
 
   return (
-    <div className="relative flex flex-col items-center text-gray-700 bg-white shadow-md w-48 rounded-xl bg-clip-border p-4 m-3 max-h-56">
+    <div className="relative flex flex-col items-center text-gray-700 bg-white shadow-md w-48 rounded-xl bg-clip-border p-4 m-3 max-h-96">
       <div className="w-28 h-28 flex justify-center items-center">
         <img className="w-full" src={image} alt={pokeName} />
       </div>
--- src/components/molecules/modal/PokemonCardModal.tsx
+++ src/components/molecules/modal/PokemonCardModal.tsx
@@ -12,7 +12,7 @@ export const PokemonCardModal: FC<PokemonCardModalType> = () => {
     <>
       {showModal ? (
         <div className="fixed top-0 left-0 z-10 w-full h-full bg-slate-700/75 flex items-center justify-center">
-          <section className=" bg-white w-1/2 h-1/2 shadow-2xl p-7 flex flex-col justify-between items-center">
+          <section className=" bg-white w-1/2 h-1/2 shadow-2xl p-7 flex flex-col justify-between items-center rounded-xl">
             <article className="flex flex-col items-start">
               <div>test this is a modal</div>
             </article>
