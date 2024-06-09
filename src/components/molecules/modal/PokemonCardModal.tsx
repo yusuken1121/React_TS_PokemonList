10236355ab5b355bdb8d55c2827c0dbc1d54f84a
yusuken1121 <yusuken1121@gmail.com>
2024-06-09 00:02:04 -0700

    💄 Improve UI of the Pokemon Modal Card

--- src/components/molecules/modal/PokemonCardModal.tsx
+++ src/components/molecules/modal/PokemonCardModal.tsx
@@ -20,21 +20,42 @@ export const PokemonCardModal: FC<PokemonCardModalType> = () => {
       {showModal ? (
         <div className="fixed top-0 left-0 z-10 w-full h-full bg-slate-700/75 flex items-center justify-center">
           <section className=" bg-white w-1/2 h-1/2 shadow-2xl p-7 flex flex-col justify-between items-center rounded-xl">
-            <article className="flex flex-col items-start">
-              <p>{contentModal!.id}</p>
-              <p className="text-3xl">{contentModal!.pokeName}</p>
-              <div className="w-20">
-                <img className="w-full" src={contentModal!.imageGif} alt="" />
+            <article className="flex flex-col items-center w-1/2">
+              <div className="w-auto h-48 object-cover flex items-center justify-center">
+                <img
+                  className="w-auto h-44 object-contain"
+                  src={contentModal!.imageGif}
+                  alt=""
+                />
               </div>
-              <div>
+              <table className="w-full border-dashed border-black border-2">
+                <tr className="tr-base"></tr>
+                <tr className="tr-base">
+                  <th className="th-base">ID:</th>
+                  <td className="td-base">{contentModal!.id}</td>
+                </tr>
+                <tr className="tr-base">
+                  <th className="th-base">Name:</th>
+                  <td className="td-base">{contentModal!.pokeName}</td>
+                </tr>
+
                 {contentModal && contentModal.types ? (
                   contentModal.types.map((types: PokemonType) => {
-                    return <p key={types.slot}>{types.type.name}</p>;
+                    return (
+                      <>
+                        <tr className="tr-base flex">
+                          <th className="th-base">Types{types.slot}:</th>
+                          <td className="td-base" key={types.slot}>
+                            {types.type.name}
+                          </td>
+                        </tr>
+                      </>
+                    );
                   })
                 ) : (
                   <></>
                 )}
-              </div>
+              </table>
             </article>
             <div className="w-40">
               <SecondaryButton onClickFunc={() => setShowModal(false)}>
